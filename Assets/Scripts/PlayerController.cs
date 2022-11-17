39963b907155856671c2ddb4fe89d07ec57e7b47
R3dL1ne <andrey.fijatov.2013@gmail.com>
2022-11-17 04:26:14 +0300

    commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 0f;
+    public float jumpForce = 0f;
+
+    Rigidbody2D rb;
+    Collider2D collider2d;
+
+    private bool isGrounded;
+    private LayerMask whatIsGround;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        collider2d = GetComponent<Collider2D>();
+    }
+
+    void Update()
+    {
+        isGrounded = Physics2D.IsTouchingLayers(collider2d, whatIsGround);
+
+        rb.velocity = new Vector2 (moveSpeed, rb.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+}
